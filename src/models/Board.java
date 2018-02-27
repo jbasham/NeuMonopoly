af01679226eca0e0b3120588f71ac2dfac9f99d6
Unknown <SRyther@student.neumont.edu>
2018-02-27 16:44:45 -0700

    Finish(?) Board and Player classes

--- src/models/Board.java
+++ src/models/Board.java
@@ -2,5 +2,6 @@ package models;
 
 public class Board
 {
-
+ public Space[] spaces = new Space[40];
+ 
 }
--- src/models/Player.java
+++ src/models/Player.java
@@ -1,6 +1,61 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Player
 {
+	HashMap<String, Property> properties =  new HashMap<>();
+	ArrayList<SpecialCard> lootBoxCards = new ArrayList<>();
+	String name;
+	int amountOfCash;
+	boolean inJail;
+	
+	public Player(String name) {
+		setName(name);
+	}
 
+	public HashMap<String, Property> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Property) {
+//		this.properties = properties;
+	}
+
+	public ArrayList<SpecialCard> getLootBoxCards() {
+		return lootBoxCards;
+	}
+
+	public void setLootBoxCards(ArrayList<SpecialCard> lootBoxCards) {
+		this.lootBoxCards = lootBoxCards;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAmountOfCash() {
+		return amountOfCash;
+	}
+
+	public void setAmountOfCash(int amountOfCash) {
+		this.amountOfCash = amountOfCash;
+	}
+
+	public boolean isInJail() {
+		return inJail;
+	}
+
+	public void setInJail(boolean inJail) {
+		this.inJail = inJail;
+	}
+	
+	public void useGetOutOfJail(){
+		
+	}
 }
