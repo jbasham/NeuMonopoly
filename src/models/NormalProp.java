e41092a8910247550fbf242cb115210643bf0ca2
Unknown <SRyther@student.neumont.edu>
2018-03-05 16:47:31 -0700

    Changed abstraction of Property and updated NormalProp rent methods

--- src/models/NormalProp.java
+++ src/models/NormalProp.java
@@ -5,7 +5,6 @@ public class NormalProp extends Property
 	private final String color;
 	private int numOfHouses;
 	private int housePrice;
-	private int houseAdditionalRent;
 
 	public NormalProp(String name, int xCoordinate, int yCoordinate, String color, int costToPurchase, int costOfRent,
 			int mortgage, int housePrice)
@@ -41,12 +40,22 @@ public class NormalProp extends Property
 
 	public int getHouseAdditionalRent()
 	{
+		int houseAdditionalRent = 0;
+		
+		//calculates extra rent to be charged based off of how many houses are on the property
+		//5 houses would be the equivalent of a hotel
+		if(getNumOfHouses() == 1) {
+			houseAdditionalRent = ((getCostOfRent()/2)*10)-getCostOfRent();
+		} else if (getNumOfHouses() == 2) {
+			houseAdditionalRent = (((getCostOfRent()/2)*10)*3)-getCostOfRent();
+		} else if (getNumOfHouses() == 3) {
+			houseAdditionalRent = (((getCostOfRent()/2)*10)*6)-getCostOfRent();
+		} else if (getNumOfHouses() == 4) {
+			houseAdditionalRent = ((((getCostOfRent()/2)*10)*6)+150)-getCostOfRent();
+		} else if (getNumOfHouses() == 5) {
+			houseAdditionalRent = ((((getCostOfRent()/2)*10)*6)+300)-getCostOfRent();
+		}
 		return houseAdditionalRent;
-	}
-
-	public void setHouseAdditionalRent(int houseAdditionalRent)
-	{
-		this.houseAdditionalRent = houseAdditionalRent;
 	}
 
 	public String getColor()
@@ -57,7 +66,13 @@ public class NormalProp extends Property
 	@Override
 	public int getRent()
 	{
-		return getCostOfRent();
+		
+		if(!this.isMortgaged()) {
+			//adds the cost of rent to the additional rent to be charged by the houses on the property
+			return (getCostOfRent() + getHouseAdditionalRent());
+		} else {
+			return 0;
+		}
 	}
 
 	@Override
